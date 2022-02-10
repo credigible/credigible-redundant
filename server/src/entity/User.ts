b6d1af6704cad0a1f13c62304a2b1db52f458afb
Rik <rik61072@gmail.com>
2022-02-10 16:48:26 +0530

    Revert "#4 (1): Implementing schema according to google docs "

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -29,78 +29,8 @@ export default class User extends BaseEntity {
   })
   lastName: string | null;
 
-  @Field()
-  @Column()
-  userName: string;
-
-  @Field()
   @Column({
     nullable: true,
   })
   age: number | null;
-
-  // not sure about this one , tried SVGElement and HTMLElement but didnt worked
-  @Column({
-    nullable: true,
-  })
-  profilePicture: string | null;
-
-  @Field()
-  @Column({
-    nullable: true,
-  })
-  phoneNumber: string | null;
-
-  @Column({
-    nullable: true,
-  })
-  educationalInstitute: string | null;
-
-  @Column({
-    nullable: true,
-  })
-  course: string | null;
-
-  @Column({
-    nullable: true,
-  })
-  state: string | null;
-
-  @Field()
-  @Column({
-    default: false,
-  })
-  ban: boolean;
-
-  @Column({
-    default: () => new Date(Date.now()),
-  })
-  userCreated: Date;
-
-  @Column({
-    default: () => new Date(Date.now()),
-  })
-  lastLogin: Date;
-
-  @Field()
-  @Column('int')
-  gender: number;
-
-  @Field()
-  @Column({
-    nullable: true,
-  })
-  upi: string | null;
-
-  @Field()
-  @Column({
-    nullable: true,
-  })
-  credit: string | null;
-
-  @Field()
-  @Column({
-    nullable: true,
-  })
-  debit: string;
 }
