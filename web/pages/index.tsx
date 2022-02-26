843a8f53fd39ace39a98d904065aa962e226b257
rik61072@gmail.com <rik61072@gmail.com>
2022-02-26 20:11:00 +0530

    linted code

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import React from 'react';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
