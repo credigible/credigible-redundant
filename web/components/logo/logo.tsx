acdabcad6311600584e3aac6bdb60f501b157439
uav <uav0108@gmail.com>
2022-02-27 23:33:22 +0530

    Created logo(Animated) component

---
+++ web/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+// import Image from 'next/image';
+import styles from './logo.module.scss';
+
+const Logo = () => (
+  <div className={styles.container}>
+    <img src="logo.svg" alt="logo" />
+    <h1>Credigible</h1>
+  </div>
+);
+
+export default Logo;
