df6a7fd8e42862c98eb38be194d74506aabd1e0f
rik61072@gmail.com <rik61072@gmail.com>
2022-02-28 10:44:08 +0530

    fixed update + update in one db call

--- server/src/Resolvers/ProfileResolver.ts
+++ server/src/Resolvers/ProfileResolver.ts
@@ -3,6 +3,7 @@ import {
   Arg,
   Ctx, Mutation, Query, Resolver, UseMiddleware,
 } from 'type-graphql';
+import { getManager } from 'typeorm';
 import User from '../entity/User';
 import { GQLAuth } from '../middlewares/Auth';
 import { AppContext } from '../types';
@@ -23,7 +24,7 @@ export default class ProfileResolver {
   @Arg('password', { nullable: true })password: string,
   @Arg('firstName', { nullable: true }) firstName: string,
   @Arg('lastName', { nullable: true }) lastName: string,
-  @Arg('username', { nullable: true }) userName: string,
+  @Arg('username', { nullable: true }) username: string,
   @Arg('email', { nullable: true }) email: string,
   @Arg('educationalInstitute', { nullable: true }) educationalInstitute: string,
   @Arg('course', { nullable: true }) course: string,
@@ -35,87 +36,25 @@ export default class ProfileResolver {
   @Arg('debit', { nullable: true }) debit: string,
   ):Promise<PasswordResetResponse> {
     try {
-      if (password) {
-        // console.log(email);
-        const hashedPassword = await hash(password, 12);
-        await User.createQueryBuilder().update({ password: hashedPassword }).where({
-          // id: user.id,
-          email: user.email,
-        }).execute();
-      }
-
-      if (firstName) {
-        await User.createQueryBuilder().update({ firstName }).where({
-          id: user.id,
-        }).execute();
-      }
-
-      if (lastName) {
-        await User.createQueryBuilder().update({ lastName }).where({
-          id: user.id,
-        }).execute();
-      }
-
-      if (userName) {
-        await User.createQueryBuilder().update({ username: userName }).where({
-          id: user.id,
-        }).execute();
-      }
-
-      if (email) {
-        await User.createQueryBuilder().update({ email }).where({
-          id: user.id,
-        });
-      }
-
-      if (educationalInstitute) {
-        await User.createQueryBuilder().update({ educationalInstitute }).where({
-          // id: user.id,
-          email: user.email,
-        });
-      }
-
-      if (course) {
-        await User.createQueryBuilder().update({ course }).where({
-          id: user.id,
-        });
-      }
-
-      if (state) {
-        await User.createQueryBuilder().update({ state }).where({
-          id: user.id,
-        });
-      }
-
-      if (age) {
-        await User.createQueryBuilder().update({ age }).where({
-          id: user.id,
-        });
-      }
-
-      if (gender) {
-        await User.createQueryBuilder().update({ gender }).where({
-          id: user.id,
-        });
-      }
-
-      if (upi) {
-        await User.createQueryBuilder().update({ upi }).where({
-          id: user.id,
-        });
-      }
-
-      if (debit) {
-        await User.createQueryBuilder().update({ debit }).where({
-          id: user.id,
-        });
-      }
-
-      if (credit) {
-        await User.createQueryBuilder().update({ gender }).where({
-          id: user.id,
-        });
-      }
+      const responseObject = {
+        password,
+        firstName,
+        lastName,
+        username,
+        email,
+        educationalInstitute,
+        course,
+        state,
+        age,
+        gender,
+        upi,
+        credit,
+        debit,
+      };
+      Object.keys(responseObject).forEach(
+        (key) => (responseObject[key] === undefined ? delete responseObject[key] : {}),
+      );
+      getManager().update(User, { id: user.id }, responseObject);
       return { status: 'success', description: 'Successfully changed details' };
     } catch (error) {
       logger.warn(error);
