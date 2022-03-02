3e0e93dfd7c66767b3239441382763d74a78cf9a
rik61072@gmail.com <rik61072@gmail.com>
2022-03-02 11:44:58 +0530

    primitive middleware for eventorg

--- server/src/middlewares/Auth.ts
+++ server/src/middlewares/Auth.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 import { AppContext, PayloadType } from '../types';
 import User from '../entity/User';
 import logger from '../utils/logger';
+import EventOrganizer from '../entity/EventOrganiser';
 
 export const GQLAuth: MiddlewareFn<AppContext> = async ({ context }, next) => {
   const header = context.req.headers.authorization;
@@ -23,6 +24,43 @@ export const GQLAuth: MiddlewareFn<AppContext> = async ({ context }, next) => {
 };
 
 export const RESTAuth = async (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers.authorization;
+  if (!header) {
+    res.status(401).json({ status: 'error', message: 'No Authorization Header' });
+    return undefined;
+  }
+
+  try {
+    const token = header.split('Bearer ')[1];
+    const payload = verify(token, process.env.JWT_SECRET_KEY) as PayloadType;
+    req.user = await User.findOne({ id: payload.userID });
+    return next();
+  } catch (err) {
+    logger.warn(err);
+    // TODO: unify error messages
+    res.status(401).json({ status: 'Error', message: 'Bad Token' });
+    return undefined;
+  }
+};
+
+export const GQLAuthEventOrganiser: MiddlewareFn<AppContext> = async ({ context }, next) => {
+  const header = context.req.headers.authorization;
+  if (!header) {
+    throw new Error('No Authorization Header');
+  }
+
+  try {
+    const token = header.split('Bearer ')[1];
+    const payload = verify(token, process.env.JWT_SECRET_KEY) as PayloadType;
+    context.user = await EventOrganizer.findOne({ id: payload.userID });
+  } catch (err) {
+    logger.warn(err);
+    throw new Error('Bad Authorization Header');
+  }
+  return next();
+};
+
+export const RESTAuthEventOrganiser = async (req: Request, res: Response, next: NextFunction) => {
   const header = req.headers.authorization;
   if (!header) {
     res.status(401).json({ status: 'error', message: 'No Authorization Header' });
