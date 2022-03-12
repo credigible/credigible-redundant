555822272d144dba5f21acd970c1c9650a3c3063
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-03-12 12:41:38 +0530

    delete user

--- server/src/controllers/Profile.ts
+++ server/src/controllers/Profile.ts
@@ -1,5 +1,6 @@
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { Request, Response } from 'express';
+import { getConnection } from 'typeorm';
 import User from '../entity/User';
 import logger from '../utils/logger';
 import EventOrganizer from '../entity/EventOrganiser';
@@ -31,5 +32,29 @@ export async function ResetPasswordEventOrganiser(req: Request, res: Response) {
   } catch (err) {
     logger.warn(err);
     res.status(500).json({ status: 'error', description: 'Something went wrong with password updation' });
+  }
+}
+
+export async function DeleteProfile(req: Request, res : Response) {
+  const reqUser = (req.user as User);
+  try {
+    if (!reqUser) {
+      throw new Error("Couldn't find user");
+    }
+
+    if (!(await compare(req.body.password, reqUser.password))) {
+      throw new Error('Wrong password');
+    }
+
+    await getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(User)
+      .where('id = :id', { id: reqUser.id })
+      .execute();
+    res.status(200).json({ status: 'success', description: 'User deleted successfully' });
+  } catch (err) {
+    logger.warn(err);
+    res.status(500).json({ status: 'error', description: 'Something went wrong with user deletion' });
   }
 }
